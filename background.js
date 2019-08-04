33e60047653430039df57570fa8049fa0fb439cd
csuZipple <1262283870@qq.com>
2019-08-04 16:13:19 +0800

    feat: 添加右键菜单：百度搜索和内搜

--- background.js
+++ background.js
@@ -3,3 +3,23 @@ chrome.runtime.onInstalled.addListener(function() {
         console.log("The color is green.");
     });
 });
+chrome.contextMenus.create({
+    id: 'baidu-search',
+    title: '使用度娘搜索：%s',
+    contexts: ['selection']
+});
+chrome.contextMenus.create({
+    id: 'baidu-int-search',
+    title: '内搜一下：%s',
+    contexts: ['selection']
+});
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    switch(info.menuItemId){
+        case 'baidu-search':
+            chrome.tabs.create({url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(info.selectionText)});
+            break;
+        case 'baidu-int-search':
+            chrome.tabs.create({url: 'http://neisou.baidu.com/new/search?word=' + encodeURI(info.selectionText)});
+            break;
+    }
+});
