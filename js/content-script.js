7b3115be078a8b0b3864fdfc82cd44b02926307a
csuZipple <1262283870@qq.com>
2019-08-04 16:55:50 +0800

    chore: 修复白名单的一个bug并添加jnject.js

--- js/content-script.js
+++ js/content-script.js
@@ -1,4 +1,16 @@
 const excludes = ['https://mp.csdn.net/mdeditor'];
+const injectJs = path => {
+    path = path || 'js/inject.js';
+    let temp = document.createElement('script');
+    temp.setAttribute('type', 'text/javascript');
+    temp.src = chrome.extension.getURL(path);
+    temp.onload = function(){
+        this.parentNode.removeChild(this);
+    };
+    document.head.appendChild(temp);
+    console.log('injected!')
+};
 document.addEventListener('mouseup', () => {
-    excludes.some(url => location.href.indexOf(url) !== -1) && window.getSelection().toString() && document.execCommand('copy');
+    excludes.some(url => location.href.indexOf(url) === -1) && window.getSelection().toString() && document.execCommand('copy');
 });
+injectJs();
