5bff4d99fdc68b0101283f781ead8be63c89a7f5
csuZipple <1262283870@qq.com>
2019-08-04 22:12:34 +0800

    feat: 阻止复制文本时出现小尾巴的问题

--- js/content-script.js
+++ js/content-script.js
@@ -8,9 +8,10 @@ const injectJs = path => {
         this.parentNode.removeChild(this);
     };
     document.head.appendChild(temp);
-    console.log('injected!')
 };
 document.addEventListener('mouseup', () => {
-    excludes.some(url => location.href.indexOf(url) === -1) && window.getSelection().toString() && document.execCommand('copy');
+    excludes.some(url => location.href.indexOf(url) === -1)
+    && window.getSelection().toString()
+    && document.execCommand('copy');
 });
 injectJs();
--- js/inject.js
+++ js/inject.js
@@ -1,1 +1,4 @@
-console.log('I was injected!');
+window.addEventListener('copy', function (e) {
+    e = window.event || e;
+    e.stopPropagation();
+}, true);
